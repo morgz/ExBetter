c87a4b0e2793c7753665401b99389d3ac9a0906f
morgz <danmorgz@googlemail.com>
2021-03-22 14:06:15 +0000

    initial commit

---
+++ lib/ex_better.ex
@@ -0,0 +1,133 @@
+defmodule ExBetter do
+  @moduledoc """
+  Documentation for `ExBetter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExBetter.hello()
+      :world
+
+  """
+  require Logger
+
+  import ExBetter.Client, only: [request: 2]
+
+  @default_user_agent "iPhone"
+  @success_codes 200..299
+
+  def client(url, token, opts \\ [])
+
+  def client(:production, token, opts),
+  do: client("https://publicapi-uk01.legendonlineservices.co.uk/", token, opts)
+
+  def client(base_url, token, opts) do
+
+    middleware = [
+      {Tesla.Middleware.BaseUrl, base_url},
+      {Tesla.Middleware.Headers, Keyword.get(opts, :headers, [])},
+      {Tesla.Middleware.Headers, [
+        {"Authorization", "Bearer " <> token },
+        {"User-Agent", @default_user_agent},
+        {"Accept-Language", "en-GB;q=1.0"}
+      ]}
+    ]
+
+    Tesla.client(middleware)
+  end
+
+  # client = ExBetter.client(:production, "token")
+  # client |> ExBetter.browse_sessions(["db9bd7d1-e741-41f2-9a99-7b4bf85cbfce"], "2021-03-29T13:24:44+0000", "2021-03-30T13:24:44+0000")
+  # {:ok, _, body} = v
+  # body |> Keyword.fetch!(:body)
+  @spec browse_sessions(
+          Tesla.Client.t(),
+          nonempty_maybe_improper_list,
+          any,
+          any,
+          any,
+          any,
+          any,
+          map
+        ) ::
+          {:error, any}
+          | {:ok, nil | binary,
+             [
+               {:method, :delete | :get | :head | :options | :patch | :post | :put | :trace}
+               | {:status, 1..1_114_111}
+               | {:url, binary},
+               ...
+             ]}
+  def browse_sessions(
+    client,
+    [_|_] = location_ids,
+    date_from,
+    date_end,
+    until_midnight_in_days \\ 1,
+    page_no \\ 1,
+    page_size \\ 10,
+    opts \\ %{}
+  ) do
+
+    url = "sessions"
+
+    params_map =
+      %{
+        locationIds: location_ids,
+        dateFrom: date_from,
+        dateEnd: date_end,
+        untilMidnightInDays: until_midnight_in_days,
+        pageNo: page_no,
+        pageSize: page_size
+      }
+      |> Map.merge(opts)
+
+      case request(
+        client,
+        method: :post,
+        url: url,
+        body: params_map
+      ) do
+        {:ok, %Tesla.Env{status: status, url: url, method: method, body: body}}
+        when status in @success_codes ->
+          {:ok, [{:url, url}, {:status, status}, {:method, method}, {:body, body}]}
+        {:ok, %Tesla.Env{body: body}} ->
+          {:error, body}
+
+        {:error, _} = other ->
+          other
+      end
+    end
+
+
+
+# client = ExBetter.client(:production, "token")
+# client |> ExBetter.get_session("ae87621e-9865-4347-acc9-f98c1e49afa2")
+  def get_session(client, session_id) do
+    url =
+      "sessions"
+      |> append_path_parameter(session_id)
+
+    case request(client, method: :get, url: url) do
+      {:ok, %Tesla.Env{status: status, body: body, url: url, method: method}}
+      when status in @success_codes ->
+        {:ok, body, [{:url, url}, {:status, status}, {:method, method}]}
+
+      {:ok, %Tesla.Env{body: body}} ->
+        {:error, body}
+
+      {:error, _} = other ->
+        other
+    end
+  end
+
+  def append_path_parameter(url, nil), do: url
+
+  def append_path_parameter(url, param) do
+    url <> "/#{param}"
+  end
+
+end
---
+++ mix.exs
@@ -0,0 +1,50 @@
+defmodule ExBetter.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :ex_better,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: package()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:tesla, "~> 1.3.0"},
+      # optional, required by JSON middleware
+      {:jason, ">= 1.0.0"},
+      {:hackney, "~> 1.14.0"},
+      {:exvcr, "~> 0.10", only: :test}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/morgz/ex_skyscanner",
+        "Changelog" => "https://github.com/morgz/ex_skyscanner/blob/master/CHANGELOG.md"
+      },
+      maintainers: ["Morgz"]
+    ]
+  end
+end
