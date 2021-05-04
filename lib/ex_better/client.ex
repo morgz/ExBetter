2794f0e4219b31afe53769f82f081e61eb376a8e
morgz <danmorgz@googlemail.com>
2021-05-04 16:36:15 +0100

    stop tesla intercepting the 400s from better

--- lib/ex_better/client.ex
+++ lib/ex_better/client.ex
@@ -22,7 +22,7 @@ defmodule ExBetter.Client do
     max_retries: 10,
     max_delay: 4_000,
     should_retry: fn
-      {:ok, %{status: status}} when status in [400, 500] -> true
+      {:ok, %{status: status}} when status in [500] -> true
       {:ok, _} -> false
       {:error, _} -> true
     end
